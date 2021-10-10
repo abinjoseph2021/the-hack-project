92bfd10654d59093fd0f404ca945106299f8d1a4
amiljoseph <92259844+amiljoseph@users.noreply.github.com>
2021-10-10 19:17:44 +0530

    Update piechart-popup.component.spec.ts

--- src/app/piechart-popup/piechart-popup.component.spec.ts
+++ src/app/piechart-popup/piechart-popup.component.spec.ts
@@ -5,20 +5,17 @@ import { PiechartPopupComponent } from './piechart-popup.component';
 describe('PiechartPopupComponent', () => {
   let component: PiechartPopupComponent;
   let fixture: ComponentFixture<PiechartPopupComponent>;
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ PiechartPopupComponent ]
     })
     .compileComponents();
   });
-
   beforeEach(() => {
     fixture = TestBed.createComponent(PiechartPopupComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
   it('should create', () => {
     expect(component).toBeTruthy();
   });
